465961fc27321cb8ade1c6ba0510bf3ddb078447
Manohar Prabhu <manoharagent13@gmail.com>
2017-07-06 14:30:59 +0530

    Removed shitty promise handling code and replaced it with async-await

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const snoowrap = require('snoowrap');
-const config = require('./config');
 const pdf = require('html-pdf');
 const moment = require('moment');
 const showdown = require('showdown');
 const converter = new showdown.Converter();
+let config;
+
+try {
+    config = require('./config');
+} catch (e) {
+    console.log('Create config.js file with the reddit script access credentials');
+    return 1;
+}
 
 const reddit = new snoowrap({
     userAgent: config.userAgent,
@@ -13,28 +20,25 @@ const reddit = new snoowrap({
     password: config.reddit_password
 });
 
-var responseCount = 0;
-
-var formatRedditToHTML = function(content) {
-    //console.log(converter.makeHtml(content));
+const formatRedditToHTML = function(content) {
     return converter.makeHtml(content);
 }
 
-var createHTMLFromContent = function(content) {
-    var html = '<!DOCTYPE html><html><body style="line-height:1.5em;color:rgba(0,0,0,.8);fill: rgba(0,0,0,.8); ">';
+const createHTMLFromContent = function(content) {
+    let html = `<!DOCTYPE html><html><body style="line-height:1.5em;color:rgba(0,0,0,.8);fill: rgba(0,0,0,.8); ">`;
     content.forEach(function(submission) {
-        html += '<div style="margin-bottom: 3em;"><div style="font-size: 24px; font-weight: bold;margin-bottom:1em;">' +
-            submission.title +
-            '</div> <div style="font-size: 16px;">' + formatRedditToHTML(submission.content) +
-            '</div></div><hr />';
+        html += `<div style="margin-bottom: 3em;"><div style="font-size: 24px; font-weight: bold;margin-bottom:1em;">
+            ${submission.title}
+            </div> <div style="font-size: 16px;"> ${formatRedditToHTML(submission.content)}
+            </div></div><hr />`;
     });
-    html += '</body></html>';
+    html += `</body></html>`;
     return html;
 }
 
 var generatePDF = function(content) {
     console.log('Generating PDF...');
-    var html = createHTMLFromContent(content);
+    const html = createHTMLFromContent(content);
     pdf.create(html, {
         "border": {
             "top": "0.1in",
@@ -48,22 +52,19 @@ var generatePDF = function(content) {
     });
 }
 
-reddit
-    .getSubreddit('WritingPrompts')
-    .getTop({ time: 'day' })
-    .then(function(submissionList) {
-        console.log('Fetched top WritingPrompts of the day...');
-        var content = [];
-        submissionList.forEach(function(submission) {
-            reddit.getSubmission(submission.id).comments.then(function(comments) {
-                if (comments.length >= 2) {
-                    content.push({ title: submission.title, content: comments[1].body });
-                }
-                responseCount++;
-                if (responseCount >= submissionList.length) {
-                    console.log('Collected WritingPrompts and the top stories...');
-                    generatePDF(content);
-                }
-            });
-        });
-    }).then(function() {});
+async function main(reddit) {
+    const submissionList = await reddit.getSubreddit('WritingPrompts').getTop({ time: 'day' });
+    console.log('Fetched top WritingPrompts of the day...');
+    const content = [];
+    console.log(`Fetching ${submissionList.length} stories...`);
+    for (let i = 0; i < submissionList.length; i++) {
+        const comments = await reddit.getSubmission(submissionList[i].id).comments;
+        if (comments.length >= 2) {
+            content.push({ title: submissionList[i].title, content: comments[1].body });
+        }
+    }
+    console.log('Collected WritingPrompts and the top stories...');
+    generatePDF(content);
+}
+
+main(reddit);
