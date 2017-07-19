d8f0c3376cff867bdf4107295a61edd398234b81
Manohar Prabhu <manoharagent13@gmail.com>
2017-07-19 15:56:25 +0530

    Added bootstrap styles to html

--- PdfGenerator.js
+++ PdfGenerator.js
@@ -18,23 +18,56 @@ const computeReadTime = function(content) {
 }
 
 const createHTMLFromContent = (content) => {
-    let html = `<!DOCTYPE html><html><body style="padding-left:10px;padding-right:10px;line-height:1.5em;color:rgba(0,0,0,.8);fill: rgba(0,0,0,.8); ">
-                <div style="padding-top:50px;padding-bottom:50px;text-align:center; width:100%;font-size: 2em;"><strong>Writing Prompts</strong></div>
+    let html = `<!DOCTYPE html><html><head><meta content="width=device-width,initial-scale=1" name=viewport><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></head>
+                <body>
+                <style>
+                    .panel-primary>.panel-heading {
+                        color: #000 !important;
+                        background-color: #EFEFEF !important;
+                        border-color: #ABABAB !important;
+                    }
+
+                    .bg-primary {
+                        background-color: #EFEFEF !important;
+                        color: #000 !important;
+                        padding: 10px !important;
+                    }
+
+                    @media print {
+                        .panel-primary>.panel-heading {
+                            color: #000 !important;
+                            background-color: #EFEFEF !important;
+                            border-color: #ABABAB !important;
+                        }
+
+                        .bg-primary {
+                            background-color: #EFEFEF !important;
+                            color: #000 !important;
+                            padding: 10px !important;
+                        }
+                    }
+
+                </style>
+                <div class="container"><h1 class="display-1">Writing Prompts</h1>
     `;
     content.writingPrompts.forEach(function(submission) {
-        html += `<div style="margin-bottom: 3em;"><div style="font-size: 24px; font-weight: bold;margin-bottom:1em;">
-            ${submission.title}
-            </div> 
+        html += `<div class="panel panel-primary">
+            <div class="panel-heading">
+            <div class="panel-title"><span class="glyphicon glyphicon-pushpin" aria-hidden="true"></span> <h3>${submission.title}</h3></div>
+            </div>
+            <div class="panel-body"> 
             <div style="font-size:14px;font-weight:bold;">${computeReadTime(submission.content)}</div>
-            <div style="font-size: 16px;"> ${formatRedditToHTML(submission.content)}
-            </div></div><hr />`;
+            <p>${formatRedditToHTML(submission.content)}</p>
+            </div>
+            </div>
+            <hr />`;
     });
 
-    html += `<div style="padding-top:200px;padding-bottom:50px;text-align:center; width:100%;font-size: 2em;"><strong>Shower Thoughts</strong></div>`
+    html += `<h1>Shower Thoughts</h1>`
     content.showerThoughts.forEach(function(thought) {
-        html += `<div style="width: 100%;padding:30px;font-style: oblique;"><span style="font-size: 1.5em">"</span>${thought}<span style="font-size: 1.5em">"</span></div><hr/>`
+        html += `<p class="bg-primary">${thought}</p>`
     });
-    html += `</body></html>`;
+    html += `</div></body></html>`;
     return html;
 }
 
@@ -45,7 +78,8 @@ var generatePDF = (content, outputPath) => {
         "border": {
             "top": "0.1in",
             "bottom": "0.1in"
-        }
+        },
+        "format": "Letter"
     }).toFile(`${outputPath}/RedditDigest-${moment().format('MMMM_Do_YYYY-HHmmss')}.pdf`, function(err, res) {
         if (err) return console.log(err);
         console.log('Written content to ' + res.filename);
